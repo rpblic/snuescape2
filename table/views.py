e87720018edb8a4cfbc83db5a63c1c0d03b43443
rpblic <rbals1218@gmail.com>
2017-10-05 16:49:37 +0900

    template#3

--- table/views.py
+++ table/views.py
@@ -3,6 +3,8 @@ from .models import Post
 from django.utils import timezone
 
 def post_list(request):
-    posts= Post.objects.filter(published_date__lte= timezone.now())
+    posts= Post.objects.all()
+    # posts= Post.objects.filter(published_date__lte= timezone.now())
+    # filter를 할 때 한 개밖에 안 나타나게 바뀌는 듯? 왜 그러지
     posts= posts.order_by('published_date')
     return render(request, 'table/post_list.html', {'posts': posts})
