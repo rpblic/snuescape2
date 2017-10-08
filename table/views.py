f2d0e26ee3ca7a4be4e134e8ff88cf6f023da53a
rpblic <rbals1218@gmail.com>
2017-10-08 01:07:53 +0000

    online setting

--- table/views.py
+++ table/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from .models import Rserv, datemake, timemake
-from django.utils import timezone
 from .forms import RservForm
 from datetime import *
 from django.contrib import messages
@@ -10,16 +9,18 @@ def timetable(request):
 
 def rserv_date(request, pk):
     date_pk= datemake(pk)
-    rservs= Rserv.objects.filter(rserv_date__year= date_pk.year,
-                         rserv_date__month= date_pk.month,
-                         rserv_date__day= date_pk.day).exclude(reserved= False)
-    rserv11= rservs.filter(rserv_date__hour= 11)
-    rserv12= rservs.filter(rserv_date__hour= 12)
-    rserv13= rservs.filter(rserv_date__hour= 13)
-    rserv14= rservs.filter(rserv_date__hour= 14)
-    rserv15= rservs.filter(rserv_date__hour= 15)
-    rserv16= rservs.filter(rserv_date__hour= 16)
-    rserv17= rservs.filter(rserv_date__hour= 17)
+    rservs= Rserv.objects.filter(rserv_date__year= date_pk.year,\
+                                rserv_date__month= date_pk.month,\
+                                rserv_date__day= date_pk.day).exclude(reserved= False)
+
+    rserv11= rservs.filter(rserv_date__hour= 11).exists()
+    rserv12= rservs.filter(rserv_date__hour= 12).exists()
+    rserv13= rservs.filter(rserv_date__hour= 13).exists()
+    rserv14= rservs.filter(rserv_date__hour= 14).exists()
+    rserv15= rservs.filter(rserv_date__hour= 15).exists()
+    rserv16= rservs.filter(rserv_date__hour= 16).exists()
+    rserv17= rservs.filter(rserv_date__hour= 17).exists()
+
     return render(request, 'table/rserv_date.html',\
                 {'rservdate': pk, 'rserv11': rserv11, 'rserv12': rserv12, 'rserv13': rserv13, 'rserv14': rserv14, 'rserv15': rserv15, 'rserv16': rserv16, 'rserv17': rserv17, })
 
