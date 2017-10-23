442445713801f8cddca6c50b889d15e2e67c8ec8
rpblic <rbals1218@gmail.com>
2017-10-23 19:11:54 +0900

    rserv_edit#1

--- table/views.py
+++ table/views.py
@@ -13,13 +13,13 @@ def rserv_date(request, pk):
                                 rserv_date__month= date_pk.month,\
                                 rserv_date__day= date_pk.day).exclude(reserved= False)
 
-    rserv11_2= rservs.filter(rserv_date__hour= 11).filter(rserv_room= 2).exists()
-    rserv12_2= rservs.filter(rserv_date__hour= 12).filter(rserv_room= 2).exists()
-    rserv13_2= rservs.filter(rserv_date__hour= 13).filter(rserv_room= 2).exists()
-    rserv14_2= rservs.filter(rserv_date__hour= 14).filter(rserv_room= 2).exists()
-    rserv15_2= rservs.filter(rserv_date__hour= 15).filter(rserv_room= 2).exists()
-    rserv16_2= rservs.filter(rserv_date__hour= 16).filter(rserv_room= 2).exists()
-    rserv17_2= rservs.filter(rserv_date__hour= 17).filter(rserv_room= 2).exists()
+    rserv11_2= rservs.filter(rserv_date__hour= 11).filter(rserv_room= 2)
+    rserv12_2= rservs.filter(rserv_date__hour= 12).filter(rserv_room= 2)
+    rserv13_2= rservs.filter(rserv_date__hour= 13).filter(rserv_room= 2)
+    rserv14_2= rservs.filter(rserv_date__hour= 14).filter(rserv_room= 2)
+    rserv15_2= rservs.filter(rserv_date__hour= 15).filter(rserv_room= 2)
+    rserv16_2= rservs.filter(rserv_date__hour= 16).filter(rserv_room= 2)
+    rserv17_2= rservs.filter(rserv_date__hour= 17).filter(rserv_room= 2)
 
     return render(request, 'table/rserv_date.html',\
                 {'rservdate': pk, 'rserv11_2': rserv11_2, 'rserv12_2': rserv12_2, 'rserv13_2': rserv13_2,\
